dd242004746633fe73d598b52c5cc9a01053a63b
learntocoder44 <160510964+learntocoder44@users.noreply.github.com>
2024-03-01 13:31:43 +0000

    commit all

--- db.js
+++ db.js
@@ -1,4 +1,17 @@
-import mongoose from 'mongoose'
+import mongoose from "mongoose";
+const mongodbURL =
+  "mongodb+srv://durgamabhilash44:wa6x5VBTtfhh8n9K@cluster0.p8fpxvc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
+mongoose.connect(mongodbURL);
 
- const mongodbURL=''
+const db = mongoose.connection;
+
+db.on("error", (err) => {
+  console.error("Error connecting to MongoDB:", err);
+});
+
+db.on("connected", () => {
+  console.log("Connected to MongoDB");
+});
+
+export default db;
--- index.js
+++ index.js
@@ -1,11 +1,47 @@
-import express from 'express';
-
+import express from "express";
+import db from "./db.js";
+import bodyParser from "body-parser";
+import Person from "./models/person.js";
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('server is runnng');
+app.use(bodyParser.json());
+app.get("/", (req, res) => {
+  res.send("server is runnng");
+});
+
+app.post("/person", async (req, res) => {
+  try {
+    const data = req.body;
+    const newPerson = new Person(data);
+
+    const response = await newPerson.save();
+
+    console.log("data saved");
+
+    res.status(200).json(response);
+  } catch (err) {
+    console.log("error internal error", err);
+
+    res.status(500).json({ err: "internal server error" });
+  }
+});
+
+app.get("/person", async (req, res) => {
+  try {
+    const data = await Person.find();
+
+    console.log("data fetched from database");
+
+    res.status(200).json(data);
+  } catch (err) {
+    console.log("data not fetched from the database", err);
+
+    res
+      .status(500)
+      .json({ err: "internal server error to fetch data from database" });
+  }
 });
 
 app.listen(3000, () => {
-  console.log('Express server initialized');
+  console.log("Express server initialized");
 });
